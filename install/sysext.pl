3ade52fb963652cf68b466eee1403584fdc36a76
John Mertz <git@john.me.tz>
2026-05-03 21:37:29 +0000

    Relatively feature complete sysext manager
    Added support for user override/additions manifest.
    Update status for installed items only.
    Allow array of extensions for install, status, update.
    Reduce some failures to warnings so other actions can still proceed.
    Fix critic complaints.

--- install/sysext.pl
+++ install/sysext.pl
@@ -72,8 +72,8 @@ sub usage() {
 }
 
 sub get_installed_version($name) {
-  my ($file, $FH) = ( "/var/lib/extensions/$name.manifest" );
-  return undef unless (-e $file);
+  my ($file) = ( "/var/lib/extensions/$name.manifest" );
+  return 0 unless (-e $file);
   confess "Failed to open manifest $file\n" unless (open($FH, '<', $file));
   while (<$FH>) {
     if ($_ =~ m/PACKAGE_VERSION=(\d.*)/) {
@@ -83,7 +83,7 @@ sub get_installed_version($name) {
     }
   }
   close($FH);
-  return undef
+  return 0;
 }
 
 sub link_from_github($ext, $repo, $type) {
@@ -104,6 +104,8 @@ sub download_latest($name) {
   my $url = $exts->{$name}->{url} or confess("Did not find repo URL for $name\n");
   
   if ($url =~ m#https://github.com/(.*)#) {
+    my $manifest = link_from_github($name, $1, 'manifest');
+    $ua->get($manifest, ':content_file' => "${EXT_DIR}/$name.manifest") || confess("Could not fetch $manifest: $!\n");
     my $squashfs = link_from_github($name, $1, 'squashfs');
     $ua->get($squashfs, ':content_file' => "${EXT_DIR}/$name.raw") || confess("Could not fetch $squashfs: $!\n");
   } elsif ($url =~ m#(:?https?://)?([^/]+)/.*#) {
@@ -111,6 +113,7 @@ sub download_latest($name) {
   } else {
     confess("Extension repo URL missing or invalid\n");
   }
+  return 1;
 }
 
 sub get_available_version($exts, $name) {
@@ -129,12 +132,11 @@ sub get_available_version($exts, $name) {
   } else {
     confess("Extension repo URL missing or invalid\n");
   }
+  return 0;
 }
 
-sub load_manifest() {
-  my $manifest = "/usr/spamtagger/etc/sysext.toml";
-  my ($FH, $toml);
-  print "Checking upstream versions...\n";
+sub load_manifest($manifest = "/usr/spamtagger/etc/sysext.toml") {
+  my ($toml);
   confess "Missing Extensions manifest $manifest\n" unless (-e $manifest);
   confess "Failed to open manifest $manifest\n" unless (open($FH, '<', $manifest));
   $toml .= $_ while (<$FH>);
@@ -165,15 +167,15 @@ sub check_pci_ids(@ids) {
   }
   foreach (@ids) {
     my ($v, $p) = $_ =~ m/^([^:]+)(?::([^:]+))?$/;
-    print "Looking for '$v':'$p'...";
     next unless (defined($pci_ids->{$v}));
     return 1 unless $p;
-    return 1 if (grep(/^$p$/, @{$pci_ids->{$v}}));
+    return 1 if (grep {/^$p$/} @{$pci_ids->{$v}});
   }
+  return 0;
 }
 
 sub check_dmi(@ids) {
-  my ($vendor, $product, $id, $FH) = ("/sys/class/dmi/id/sys_vendor", "/sys/class/dmi/id/product_name");
+  my ($vendor, $product, $id) = ("/sys/class/dmi/id/sys_vendor", "/sys/class/dmi/id/product_name");
   return 0 unless (-e $vendor);
   if (open($FH, '<', $vendor)) {
     $id .= $_ while (<$FH>);
@@ -195,10 +197,11 @@ sub check_dmi(@ids) {
     }
     return 1 if ($p eq $prod);
   }
+  return 0;
 }
 
 sub check_device_tree(@ids) {
-  my ($compatible, $id, $FH) = ("/proc/device-tree/compatible");
+  my ($compatible, $id) = ("/proc/device-tree/compatible");
   return 0 unless (-e $compatible);
   if (open($FH, '<', $compatible)) {
     $id .= $_ while (<$FH>);
@@ -209,6 +212,7 @@ sub check_device_tree(@ids) {
     next if ($_ eq '');
     return 1 if ($id =~ m/^$_/);
   }
+  return 0;
 }
 
 sub check_virt_env(@ids) {
@@ -218,19 +222,24 @@ sub check_virt_env(@ids) {
   foreach (@ids) {
     return 1 if ($_ eq $virt);
   }
+  return 0;
 }
 
-sub get_status($ext = undef) {
-  my ($FH, $toml, $e);
+sub get_status(@e) {
+  my ($toml, $e, $custom);
   if (-e $cache) {
     if (open($FH, '<', $cache)) {
       $toml .= $_ while (<$FH>);
       close($FH);
       if ($toml) {
-        confess "Failed to parse TOML from $cache\n" unless ($e = from_toml($toml));
+        confess "Failed to parse TOML from $cache\n" unless ($exts = from_toml($toml));
       }
-      if (defined($e->{timestamp}) && $e->{timestamp} > (time() - 86400)) {
-        return $e;
+      if (scalar(@e)) {
+        if (ref($exts)) {
+          $e = $exts;
+        }
+      } elsif (defined($exts->{timestamp}) && $exts->{timestamp} > (time() - 86400)) {
+        return $exts;
       } else {
         print "Cache is out of date. ";
       }
@@ -238,14 +247,20 @@ sub get_status($ext = undef) {
       print "Cannot read cache file $cache\n";
     }
   }
-  $e = load_manifest();
+  if (!defined($e)) {
+    $e = load_manifest();
+    $custom = load_manifest("/etc/spamtagger/etc/sysext.toml") if (-e "/etc/spamtagger/etc/sysext.toml");
+    $e->{$_} = $custom->{$_} foreach (keys(%{$custom}));
+  }
+
   my $json = `systemd-sysext status --json=short`;
   chomp($json);
   my $ret = decode_json($json) || confess("Could not parse current systemd-sysext status: $json\n");
   foreach my $hierarchy (@{$ret}) {
     push(@active, @{$hierarchy->{extensions}}) unless ($hierarchy->{extensions} eq "none");
   }
-  foreach my $ext (keys(%{$e})) {
+  @e = keys(%{$e}) unless (scalar(@e));
+  foreach my $ext (@e) {
     my $installed = is_installed($ext);
     if ($installed) {
       $e->{$ext}->{version} = get_installed_version($ext);
@@ -282,22 +297,22 @@ sub is_installed($ext) {
   }
   if (-e "/var/lib/extensions/${ext}.raw") {
     if (scalar(@active) == 0) {
-      return 2 if (scalar(@installed) && scalar(grep(/^${ext}/, @installed)));
+      return 2 if (scalar(@installed) && scalar(grep {/^${ext}/} @installed));
       return 3;
-    } elsif (grep(/^${ext}$/, @active)) {
+    } elsif (grep {/^${ext}$/} @active) {
       return 1;
     } else {
-      return 2 if (scalar(@installed) && scalar(grep(/^${ext}/, @installed)));
+      return 2 if (scalar(@installed) && scalar(grep {/^${ext}/} @installed));
       return 3;
     }
   }
 }
 
-sub print_status($ext = undef) {
+sub print_status(@list) {
   printf("+%s+%s+%s+%s+%s+\n", "-"x22, "-"x16, "-"x20, "-"x20, "-"x14);
   printf("| %-20s | %-14s | %-18s | %-18s | %-12s |\n", "Extension", "Status", "Installed", "Available", "Recommended");
   printf("+%s+%s+%s+%s+%s+\n", "-"x22, "-"x16, "-"x20, "-"x20, "-"x14);
-  my @list = ( $ext ) || sort(keys(%{$exts}));
+  @list = sort(keys(%{$exts})) unless (scalar(@list));
   foreach my $name (@list) {
     next unless (ref($exts->{$name}));
     printf(
@@ -309,37 +324,97 @@ sub print_status($ext = undef) {
     );
   }
   printf("+%s+%s+%s+%s+%s+\n", "-"x22, "-"x16, "-"x20, "-"x20, "-"x14);
+  return 0;
 }
 
-usage() if (!defined($ARGV[0]) || $ARGV[0] eq "-h" || $ARGV[0] eq "help" );
-$exts = get_status();
-if ($ARGV[0] eq "-s" || $ARGV[0] eq "status" ) {
-  print_status($ARGV[1]);
-} elsif ($ARGV[0] eq "-u" || $ARGV[0] eq "update" ) {
-  update();
-} elsif ($ARGV[0] eq "-a" || $ARGV[0] eq "auto" ) {
-  recommended();
-} elsif ($ARGV[0] eq "-i" || $ARGV[0] eq "install" ) {
-  download_latest($ARGV[1]);
-  $exts = get_status();
-  if ($exts->{$ARGV[1]}->{status} ne "inactive") {
-    confess("Extension not found after download");
+sub install(@e) {
+  my (@downloaded, @failed);
+  foreach my $ext (@e) {
+    if (!defined($exts->{$ext})) {
+      print STDERR "No matching extension $ext\n";
+      next;
+    } elsif ($exts->{$ext}->{status} eq 'inactive') {
+      push(@downloaded, $ext);
+      next;
+    } elsif ($exts->{$ext}->{status} eq 'active') {
+      next;
+    }
+    download_latest($ext);
+    $exts = get_status($ext);
+    if ($exts->{$ext}->{status} eq 'inactive') {
+      push(@downloaded, $ext);
+    } else {
+      print STDERR "$ext not loadable after download\n";
+    }
   }
-} elsif ($ARGV[0] eq "-r" || $ARGV[0] eq "remove" ) {
-  if ($exts->{$ARGV[1]}->{status} eq "active") {
-    `systemd-sysext unmerge`;
-    unlink("$EXT_DIR/$ARGV[1].raw");
-    unlink("$EXT_DIR/$ARGV[1].manifest");
-    `systemd-sysext refresh`;
-    `systemd-sysext merge`;
-  } elsif (-e "$EXT_DIR/$ARGV[1].raw") {
-    unlink("$EXT_DIR/$ARGV[1].raw");
-    unlink("$EXT_DIR/$ARGV[1].manifest");
-  } else {
-    confess("$ARGV[1] is not installed");
+  `systemd-sysext merge`;
+  foreach (@downloaded) {
+    $exts = get_status($_);
+    push(@failed, $_) if ($exts->{$_}->{status} ne "active");
   }
+  print STDERR (join(', ', @failed)." failed to activate after download\n") if (scalar(@failed));
+  print_status(@e);
+  return 0;
+}
+
+sub remove(@e) {
+  foreach my $ext (@e) {
+    if ($exts->{$ext}->{status} eq "active") {
+      `systemd-sysext unmerge`;
+      unlink("$EXT_DIR/$ext.raw");
+      unlink("$EXT_DIR/$ext.manifest");
+      `systemd-sysext refresh`;
+      `systemd-sysext merge`;
+    } elsif (-e "$EXT_DIR/$ext.raw") {
+      unlink("$EXT_DIR/$ext.raw");
+      unlink("$EXT_DIR/$ext.manifest");
+    } else {
+      print("$ext is not installed");
+    }
+  }
+  return 0;
+}
+
+my $action = shift(@ARGV) || usage();
+usage() if ($action eq "-h" || $action eq "help" );
+if ($action eq "-s" || $action eq "status" ) {
   $exts = get_status();
-  print_status($ARGV[1]);
+  print_status(@ARGV);
+} elsif ($action eq "-u" || $action eq "update" ) {
+  unlink($cache) if (-e $cache);
+  $exts = get_status();
+  my @update;
+  foreach my $ext (keys(%{$exts})) {
+    next unless (ref($exts->{$ext}));
+    if ($exts->{$ext}->{status} =~ /(in)?active/) {
+      if ($exts->{$ext}->{version} ne $exts->{$ext}->{available}  ) {
+        push(@update, $ext);
+      }
+    }
+  }
+  install(@update);
+} elsif ($action eq "-a" || $action eq "auto" ) {
+  unlink($cache) if (-e $cache);
+  $exts = get_status();
+  my @install;
+  foreach my $ext (keys(%{$exts})) {
+    next unless (ref($exts->{$ext}));
+    if ($exts->{$ext}->{recommended}) {
+      if ($exts->{$ext}->{status} !~ /(in)?active/) {
+        push(@install, $ext);
+      }
+    }
+  }
+  install(@install);
+} elsif ($action eq "-i" || $action eq "install" ) {
+  unlink($cache) if (-e $cache);
+  $exts = get_status();
+  install(@ARGV);
+} elsif ($action eq "-r" || $action eq "remove" ) {
+  $exts = get_status();
+  remove(@ARGV);
+  $exts = get_status();
+  print_status(@ARGV);
 } else {
-  usage()
+  usage();
 }
